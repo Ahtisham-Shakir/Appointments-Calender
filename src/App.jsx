7a2758ef58c7c289442af35cf788a26ae989ec43
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-10-05 15:52:16 +0500

    changing the logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 const App = () => {
